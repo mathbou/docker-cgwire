13a6056b8f88768f1bdad233d30de93ed4d90e31
mathieu <mathieu.bouzard@free.fr>
2019-07-08 22:38:28 +0200

    refactoring

--- get_build_dependencies.sh
+++ get_build_dependencies.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-echo "GET DEPENDENCIES"
+source ./common.sh
+
+echo "${BLUE}GET KITSU"
 git clone -b kitsu --single-branch --depth 1 https://gitlab.com/mathbou/docker-cgwire.git ./kitsu || git -C ./kitsu pull
+
+echo "${BLUE}GET ZOU"
 git clone -b zou --single-branch --depth 1 https://gitlab.com/mathbou/docker-cgwire.git ./zou || git -C ./zou pull
