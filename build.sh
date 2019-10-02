8f77417f5d433cd99cb35a2b98897bb979adffdb
mathieu <mathieu.bouzard@free.fr>
2019-10-02 02:20:25 +0200

    Fix: anti-slash escaope in jq test

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ function build_images() {
     command -v jq 1>/dev/null || ( echo "${ERROR}jq required" && exit 1 )
 
     if [[ $KITSU_VERSION == "latest" ]]; then
-        export KITSU_VERSION=`curl https://api.github.com/repos/cgwire/kitsu/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\.[0-9]+)+"))?' | grep -m1 ""`
+        export KITSU_VERSION=`curl https://api.github.com/repos/cgwire/kitsu/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\\\.[0-9]+)+"))?' | grep -m1 ""`
         echo "${GREEN}Set KITSU_VERSION to $KITSU_VERSION"
-    elif [[ $ZOU_VERSION == "latest" ]]; then
-        export ZOU_VERSION=`curl https://api.github.com/repos/cgwire/zou/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\.[0-9]+)+"))?' | grep -m1 ""`
+    fi
+    if [[ $ZOU_VERSION == "latest" ]]; then
+        export ZOU_VERSION=`curl https://api.github.com/repos/cgwire/zou/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\\\.[0-9]+)+"))?' | grep -m1 ""`
         echo "${GREEN}Set ZOU_VERSION to $ZOU_VERSION"
     fi
 
