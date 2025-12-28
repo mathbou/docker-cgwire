f333a45bf7207c6d6519910a42223d5d08f6fd2d
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2025-12-28 16:47:01 +0100

    🩹 silent curl

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SWD=$(cd $(dirname $0);echo $PWD)
 
 function get_kitsu_version() {
     if [[ $KITSU_VERSION == "latest" ]]; then
-        export KITSU_VERSION=`curl https://api.github.com/repos/cgwire/kitsu/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\\\.[0-9]+){2}"))?' | grep -m1 ""`
+        export KITSU_VERSION=`curl -s https://api.github.com/repos/cgwire/kitsu/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\\\.[0-9]+){2}"))?' | grep -m1 ""`
         echo "${GREEN}Set KITSU_VERSION to $KITSU_VERSION"
     fi
 }
@@ -12,7 +12,7 @@ function get_kitsu_version() {
 
 function get_zou_version(){
     if [[ $ZOU_VERSION == "latest" ]]; then
-        export ZOU_VERSION=`curl https://api.github.com/repos/cgwire/zou/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\\\.[0-9]+){2}"))?' | grep -m1 ""`
+        export ZOU_VERSION=`curl -s https://api.github.com/repos/cgwire/zou/commits | jq -r '.[].commit.message | select(. | test("[0-9]+(\\\\.[0-9]+){2}"))?' | grep -m1 ""`
         echo "${GREEN}Set ZOU_VERSION to $ZOU_VERSION"
     fi
 }
