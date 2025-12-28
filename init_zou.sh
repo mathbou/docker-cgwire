20dd6d52246058ecd53731386d5d02a25b7e435d
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2025-12-28 21:39:00 +0100

    🐛 disable telemetry if DEBUG enabled

--- init_zou.sh
+++ init_zou.sh
@@ -5,6 +5,11 @@ export LANG=C.UTF-8
 
 mkdir -p ${PREVIEW_FOLDER} ${TMP_DIR}
 
-zou upgrade-db
+if [ ${DEBUG:-0} -eq 1 ]; then
+  zou upgrade-db --no-telemetry
+else
+  zou upgrade-db
+fi
+
 zou init-data
 zou create-admin admin@example.com --password mysecretpassword
--- upgrade_zou.sh
+++ upgrade_zou.sh
@@ -3,4 +3,8 @@
 export LC_ALL=C.UTF-8 
 export LANG=C.UTF-8
 
-zou upgrade-db
+if [ ${DEBUG:-0} -eq 1 ]; then
+  zou upgrade-db --no-telemetry
+else
+  zou upgrade-db
+fi
